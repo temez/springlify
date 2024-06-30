8391e44fc813a9f415c257bd783ebfc4966e8637
temez <temezf@gmail.com>
2024-06-30 19:53:26 +0300

    Moved MiniMessage and LegacyComponentSerializer initialization to KyoriAdventureConfiguration

--- springlify-platform/springlify-platform-commons/src/main/java/dev/temez/springlify/platform/text/converter/LegacyTextConverter.java
+++ springlify-platform/springlify-platform-commons/src/main/java/dev/temez/springlify/platform/text/converter/LegacyTextConverter.java
@@ -2,6 +2,7 @@ package dev.temez.springlify.platform.text.converter;
 
 import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
 import net.kyori.adventure.text.Component;
@@ -32,11 +33,12 @@ import java.util.stream.Collectors;
 @ConditionalOnExpression(
     "'${springlify.platform.text-converter-mode}'.equalsIgnoreCase('legacy')"
 )
+@RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public final class LegacyTextConverter implements TextConverter {
 
   @NotNull
-  LegacyComponentSerializer serializer = LegacyComponentSerializer.legacyAmpersand();
+  LegacyComponentSerializer serializer;
 
   /**
    * Logs a debug message upon initialization.
