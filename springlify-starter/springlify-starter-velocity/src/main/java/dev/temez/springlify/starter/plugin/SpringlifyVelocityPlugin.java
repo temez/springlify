2509c12811d883fc45f666cee42bc2d1516a3906
temez <temezf@gmail.com>
2024-07-01 19:37:12 +0300

    Added springlify-starter-velocity

---
+++ springlify-starter/springlify-starter-velocity/src/main/java/dev/temez/springlify/starter/plugin/SpringlifyVelocityPlugin.java
@@ -0,0 +1,60 @@
+package dev.temez.springlify.starter.plugin;
+
+import com.google.common.eventbus.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
+import com.velocitypowered.api.proxy.ProxyServer;
+import dev.temez.springlify.starter.configuration.loader.ConfigurationLoader;
+import dev.temez.springlify.starter.configuration.loader.ConfigurationLoaderImpl;
+import dev.temez.springlify.starter.initializer.SpringlifyInitializer;
+import dev.temez.springlify.starter.initializer.SpringlifyInitializerImpl;
+import dev.temez.springlify.starter.initializer.loader.ClassLoaderFactory;
+import dev.temez.springlify.starter.initializer.loader.CompoundClassLoaderFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.nio.file.Path;
+
+@Setter
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PROTECTED)
+public class SpringlifyVelocityPlugin implements SpringlifyPlugin {
+
+  @NotNull
+  ClassLoaderFactory classLoaderFactory = new CompoundClassLoaderFactory();
+
+  @NotNull
+  ConfigurationLoader configurationLoader = new ConfigurationLoaderImpl();
+
+  @NotNull
+  SpringlifyInitializer initializer = new SpringlifyInitializerImpl(classLoaderFactory, configurationLoader);
+
+  @NonFinal
+  ConfigurableApplicationContext context;
+
+  @NotNull
+  ProxyServer server;
+
+  @NotNull
+  Path dataFolder;
+
+  public SpringlifyVelocityPlugin(@NotNull ProxyServer server, @NotNull Path dataFolder) {
+    this.server = server;
+    this.dataFolder = dataFolder;
+  }
+
+  @Subscribe
+  public void onProxyInitialization(@NotNull ProxyInitializeEvent event) {
+    initialize();
+  }
+
+  @Subscribe
+  public void onProxyShutdown(@NotNull ProxyShutdownEvent event) {
+    shutdown();
+  }
+}
